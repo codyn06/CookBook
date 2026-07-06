15dfaf7356aeefdb9220c808f8677c32d3bb1ad0
connor-tomelden <tomeldenconnor@gmail.com>
2026-07-06 17:14:05 -0400

    Added the hash function for inverted hashmap

--- src/structures/hashmap/HashMap.cpp
+++ src/structures/hashmap/HashMap.cpp
@@ -1,1 +1,11 @@
 #include "HashMap.h"
+
+int HashMap::hashFunction(const std::string &ingredient) {
+
+    unsigned int hash_value = 0;
+    // Hash value = multiply current hash value by 31 (base 31) and add the ASCII value of the char in the string
+    for (char c: ingredient) {
+        hash_value = (hash_value * 31) + c;
+    }
+    return hash_value % capacity;
+}
