baf620c627e2ec99fdfaaf86f23255b694c0ceaf
Cody <nguyencody2006@gmail.com>
2026-07-06 17:03:27 -0400

    Added declarations to hashmap class

--- src/structures/hashmap/HashMap.h
+++ src/structures/hashmap/HashMap.h
@@ -2,10 +2,25 @@
 #define HASHMAP_H
 
 #include "../Structure.h"
+
+struct HashNode
+{
+    std::string ingredient;
+    std::vector<int> recipeIds;
+    HashNode *next;
+
+    HashNode(std::string i) : ingredient(i), next(nullptr) {}
+};
+
 class HashMap : public Structure
 {
+private:
+    int hashFunction(const std::string &ingredient);
+
 public:
     HashMap(std::vector<Recipe> &recipes);
+    void insert(const std::string &ingredient, int recipeId);
+    std::vector<int> search(const std::string &ingredient);
     std::vector<Recipe> query(const std::vector<std::string> &pantry) override;
 };
 
