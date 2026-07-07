643c8824f8cefc491f382cec92f78b9c486b0582
Cody <nguyencody2006@gmail.com>
2026-07-07 14:01:38 -0400

    Defined graph constructor

--- src/structures/graph/Graph.cpp
+++ src/structures/graph/Graph.cpp
@@ -1,1 +1,16 @@
 #include "Graph.h"
+
+Graph::Graph(const std::vector<Recipe> &recipes)
+{
+    recipeDegrees.resize(recipes.size(), 0);
+
+    for (const auto &r : recipes)
+    {
+        recipeDegrees[r.id] = r.ner.size();
+
+        for (const auto &ingredient : r.ner)
+        {
+            adjacencyList[ingredient].push_back(r.id);
+        }
+    }
+}
