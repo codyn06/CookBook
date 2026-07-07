024135524626f127333284dc455da3b694b9d7cb
Cody <nguyencody2006@gmail.com>
2026-07-07 13:58:37 -0400

    Declared graph class member variables

--- src/structures/graph/Graph.h
+++ src/structures/graph/Graph.h
@@ -2,9 +2,15 @@
 #define GRAPH_H
 
 #include "../Structure.h"
+#include <map>
 class Graph : public Structure
 {
-    Graph(std::vector<Recipe> &recipes);
+private:
+    std::map<std::string, std::vector<int>> adjacencyList;
+    std::vector<int> recipeDegrees;
+
+public:
+    Graph(const std::vector<Recipe> &recipes);
     std::vector<Recipe> query(const std::vector<std::string> &pantry) override;
 };
 
