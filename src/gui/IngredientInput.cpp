e8ec0afec1fb521884ff7962c2d521ab1b5647a4
Cody <nguyencody2006@gmail.com>
2026-07-09 19:05:13 -0400

    Added onAddIngredientText() functionality

--- src/gui/IngredientInput.cpp
+++ src/gui/IngredientInput.cpp
@@ -1,6 +1,30 @@
 #include "IngredientInput.h"
+#include <algorithm>
 
 IngredientInput::IngredientInput(QWidget *parent) : QWidget(parent)
 {
     setupUi();
+}
+
+void IngredientInput::setupUi() {}
+
+void IngredientInput::onAddIngredientText()
+{
+    std::string text = ingredientTextField->text().trimmed().toLower().toStdString();
+
+    // Edge Cases
+    if (text.empty())
+    {
+        return;
+    }
+
+    if (std::find(selectedIngredients.begin(), selectedIngredients.end(), text) != selectedIngredients.end())
+    {
+        ingredientTextField->clear();
+        return;
+    }
+
+    selectedIngredients.push_back(text);
+    addIngredientTag(text);
+    ingredientTextField->clear();
 }
