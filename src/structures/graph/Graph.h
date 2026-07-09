690c4f786bdec425c4123d46afcfcddfa9dd2a1e
chao-j <166747975+chao-j@users.noreply.github.com>
2026-07-08 21:36:38 -0400

    Update Graph.h

--- src/structures/graph/Graph.h
+++ src/structures/graph/Graph.h
@@ -2,10 +2,20 @@
 #define GRAPH_H
 
 #include "../Structure.h"
+#include "../../data/Recipe.h"
+#include <unordered_map>
+#include <string>
+#include <vector>
+
 class Graph : public Structure
 {
-    Graph(std::vector<Recipe> &recipes);
-    std::vector<Recipe> query(const std::vector<std::string> &pantry) override;
+private:
+    std::unordered_map<std::string, std::vector<int>> adjacencyList;
+    std::unordered_map<int, Recipe> recipeLookup;
+
+public:
+    Graph(const std::vector<Recipe> &recipes);
+    std::vector<RecipeMatch> query(const std::vector<std::string> &pantry) override;
 };
 
 #endif // GRAPH_H
