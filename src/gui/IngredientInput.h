73ab7eff8acf146c2d8b4d1cdc248f30ec27b1ef
Cody <nguyencody2006@gmail.com>
2026-07-09 17:45:01 -0400

    Declared IngredientInput

--- src/gui/IngredientInput.h
+++ src/gui/IngredientInput.h
@@ -1,4 +1,42 @@
 #ifndef INGREDIENT_INPUT_H
 #define INGREDIENT_INPUT_H
 
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <vector>
+#include <string>
+
+class IngredientInput : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit IngredientInput(QWidget *parent = nullptr);
+    std::vector<std::string> getSelectedIngredients() const;
+
+signals:
+    void ingredientsSubmitted(const std::vector<std::string> &pantry);
+
+private slots:
+    void onAddIngredientText();
+    void onChipToggled(const QString &ingredient, bool checked);
+    void onNextClicked();
+
+private:
+    void setupUi();
+    void addChip(const QString &ingredient);
+    void addIngredientToList(const std::string &ingredient);
+    void removeIngredientFromList(const std::string &ingredient);
+
+    QLineEdit *ingredientTextField;
+    QPushButton *nextButton;
+    QHBoxLayout *chipLayout;
+    QVBoxLayout *selectedListLayout;
+
+    std::vector<std::string> selectedIngredients;
+};
+
 #endif // INGREDIENT_INPUT_H
