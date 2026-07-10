4c2993fd0566739ef9789afec8e71d27a53b557c
connor-tomelden <tomeldenconnor@gmail.com>
2026-07-10 09:48:02 -0400

    Refactor RecipeCard declaration so it inherits from Recipe Match struct.

--- src/gui/RecipeCard.h
+++ src/gui/RecipeCard.h
@@ -1,17 +1,18 @@
 #ifndef RECIPECARD_H
 #define RECIPECARD_H
 
-#include <QLayout>
-#include "data/Recipe.h" 
+#include <QFrame>
+#include <QVBoxLayout>
+#include <QLabel>
+#include "structures/Structure.h" 
 
 class RecipeCard : public QFrame {
     Q_OBJECT
 
 public:
-    explicit RecipeCard(const Recipe& recipe, QWidget *parent = nullptr);
-
+    explicit RecipeCard(const RecipeMatch& match, QWidget *parent = nullptr);
 private:
-    void setupUi(const Recipe& recipe);
+    void setupUi(const RecipeMatch& match);
 };
 
 #endif // RECIPECARD_H
