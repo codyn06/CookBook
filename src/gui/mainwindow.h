2c503a9a402bf92e91bdb3fe985d7a11a635ef28
connor-tomelden <tomeldenconnor@gmail.com>
2026-07-10 14:48:58 -0400

    main window constructor declaration

---
+++ src/gui/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QStackedWidget>
+#include <vector>
+#include <string>
+
+#include "IngredientInput.h"
+#include "StructureSelect.h"
+#include "ResultsScreen.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() = default;
+
+private slots:
+    void handleIngredientsSubmitted(const std::vector<std::string> &ingredients);
+    void handleStructureSelected(int selection);
+
+private:
+    QStackedWidget *stackedWidget;
+    
+    IngredientInput *inputScreen;
+    StructureSelect *structureScreen;
+    ResultsScreen *resultsScreen;
+
+    std::vector<std::string> currentIngredients;
+};
+
+#endif // MAINWINDOW_H
