f82d96db4f9a497411e0a62ec063b5c4384def26
connor-tomelden <tomeldenconnor@gmail.com>
2026-07-10 15:51:55 -0400

    simple loading screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QSplashScreen>
+#include <QPixmap>
 
 #include "gui/mainwindow.h"
 #include "data/data.h"
@@ -9,6 +11,16 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    QPixmap pixmap(500, 300);
+    pixmap.fill(Qt::white);
+    
+    QSplashScreen splash(pixmap);
+    
+    splash.show();
+    splash.showMessage("Loading Recipes... Please wait.", Qt::AlignCenter, Qt::black);
+    
+    app.processEvents();
 
     std::vector<Recipe> recipes = loadRecipes("../dataset/full_dataset.csv");
 
