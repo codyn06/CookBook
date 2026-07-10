d4b49a66796adcb56a4a3082b7459456294288b2
connor-tomelden <tomeldenconnor@gmail.com>
2026-07-10 09:52:05 -0400

    RecipeCard Constructor

--- src/gui/RecipeCard.cpp
+++ src/gui/RecipeCard.cpp
@@ -1,1 +1,14 @@
 #include "RecipeCard.h"
+#include <QString>
+#include <QStringList>
+
+RecipeCard::RecipeCard(const RecipeMatch& match, QWidget *parent) 
+    : QFrame(parent) {
+    
+    this->setFrameShape(QFrame::StyledPanel);
+    this->setStyleSheet("RecipeCard { border: 1px solid black; border-radius: 8px; background-color: white; }");
+    this->setFixedSize(220, 350); 
+
+    setupUi(match);
+}
+
