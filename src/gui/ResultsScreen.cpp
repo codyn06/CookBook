a7aaa2f3cc2ba5e59e20d5617a38c2ff3354ea98
connor-tomelden <tomeldenconnor@gmail.com>
2026-07-11 10:08:58 -0400

    ResultsScreen doc

--- src/gui/ResultsScreen.cpp
+++ src/gui/ResultsScreen.cpp
@@ -3,14 +3,17 @@
 #include <QScrollArea>
 
 ResultsScreen::ResultsScreen(QWidget *parent) : QWidget(parent) {
+    // Set the ResultsScreen window to a fixed size so RecipeCards are formatted 
     this->setFixedSize(960,800);
     
     mainLayout = new QVBoxLayout(this);
 
+    // Home button that routes back to ingredient input screen
     homeButton = new QPushButton("Home", this);
     homeButton->setFixedSize(90, 35);
     connect(homeButton, &QPushButton::clicked, this, &ResultsScreen::homeRequested);
 
+    // Top Recipes header
     headerLabel = new QLabel("Top Recipes:", this);
     QFont headerFont = headerLabel->font();
     headerFont.setPointSize(24); 
@@ -20,6 +23,7 @@ ResultsScreen::ResultsScreen(QWidget *parent) : QWidget(parent) {
     mainLayout->addWidget(homeButton, 0, Qt::AlignLeft);
     mainLayout->addWidget(headerLabel);
 
+    // Scroll bar to see all recipes 
     QScrollArea *scrollArea = new QScrollArea(this);
     scrollArea->setWidgetResizable(true); 
     scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff); 
@@ -39,6 +43,7 @@ ResultsScreen::ResultsScreen(QWidget *parent) : QWidget(parent) {
 
 void ResultsScreen::setRecipes(const std::vector<RecipeMatch> &matches) {
     QLayoutItem *child;
+    // Clear the old recipe cards if needed
     while ((child = cardsLayout->takeAt(0)) != nullptr) {
         if (child->widget()) {
             delete child->widget(); 
@@ -46,6 +51,7 @@ void ResultsScreen::setRecipes(const std::vector<RecipeMatch> &matches) {
         delete child; 
     }
 
+    // Handles case if user's ingredient input does not match to any recipes.
     if (matches.empty()) {
         QLabel *noResultsLabel = new QLabel("No recipes found. Try adding or adjusting your ingredients!", this);
         
